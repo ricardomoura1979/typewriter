1ffae7a7ff6b1b5c35cd4bd5cf1c3c8e9e3fa410
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-07 18:34:08 +0000

    added miliseconds to make a print working as typewriter

--- typewriter.js
+++ typewriter.js
@@ -4,7 +4,7 @@ for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, time); // <= 1s delay to make it noticeable. Can dial it down later.
-  time += 50
+  time += 450
 }
 
 
